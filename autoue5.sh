e211c447e3d5be0ff8a8fe423cc3fbee96fd86e4
serv3r0 <107516180+serv3r0@users.noreply.github.com>
2022-06-16 17:19:36 +0200

    Update autoue5.sh

--- autoue5.sh
+++ autoue5.sh
@@ -18,13 +18,13 @@ echo " ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀     
 
 echo "UnrealEngine5 auto install script will run in 5 seconds , press CTRL+C to cancel"
 sleep 1
-echo "-->4"
+echo "▄ ▄ ▄ ▄"
 sleep 1
-echo "--->3"
+echo "▄ ▄ ▄"
 sleep 1
-echo "---->2"
+echo "▄ ▄"
 sleep 1
-echo "----->1"
+echo "▄"
 sleep 1
 echo "starting auto inst script"
 
@@ -33,19 +33,37 @@ cd /home/$usr/Documents
 echo "cloning repo"
 git clone https://github.com/EpicGames/UnrealEngine.git
 echo "getting into cloned folder"
-cd /UnrealEngine
+cd /home/$usr/Documents/UnrealEngine
 sleep 5
 echo "running scripts"
 sudo ./setup.sh
 sleep 5
 sudo ./GenerateProjectFiles.sh
-echo "if you got an error use - sudo ln -s /etc/ssl /usr/lib/ssl and procced with manual install" 
 echo "compiling"
 sleep 5
 sudo make
-echo "starting UnrealEngine"
+while true; do
+    read -p "Do you wish to install unrealeditor from everywhere script? Yes or No" yn
+    case $yn in
+        [Yy]* ) mkdir /home/$usr/scripts
+        cd /home/$usr/Downloads
+        git clone https://github.com/serv3r0/autoue5.git
+        mv /home/$usr/Downloads/autoue5/unrealeditor.sh /home/$usr/scripts
+        echo "Add path to .bashrc at your home dir, export PATH="/home/$usr/scripts:$PATH""
+        sleep 1
+        echo "starting UnrealEngine"
 echo "in future , to start UnrealEditor just go into /Engine/Binaries/Linux and run UnrealEditor"
 sudo chmod 700 ../UnrealEngine-rekease
 sudo chown $usr -R ../UnrealEngine-release
-cd /Engine/Binaries/Linux
+cd /home/$usr/Documents/UnrealEngine/Engine/Binaries/Linux
 ./UnrealEditor
+        ; break;;
+        [Nn]* )  echo "starting UnrealEngine"
+echo "in future , to start UnrealEditor just go into /Engine/Binaries/Linux and run UnrealEditor"
+sudo chmod 700 ../UnrealEngine-rekease
+sudo chown $usr -R ../UnrealEngine-release
+cd /home/$usr/Documents/UnrealEngine/Engine/Binaries/Linux
+./UnrealEditor;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
